0db9e010aee0877bde7aad6b6f6946f0559945bb
Md Adit Hasan <skadithasan19@gmail.com>
2025-01-18 08:54:51 -0600

    Initial Commit

---
+++ FlickrIMG/FlickrIMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrIMGApp.swift
+//  FlickrIMG
+//
+//  Created by Adit Hasan on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickrIMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrIMGTests/FlickrIMGTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrIMGTests.swift
+//  FlickrIMGTests
+//
+//  Created by Adit Hasan on 1/18/25.
+//
+
+import Testing
+@testable import FlickrIMG
+
+struct FlickrIMGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
